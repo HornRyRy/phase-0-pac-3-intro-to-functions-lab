0567417cb3dc688551c653c8e9bbe9b9ece59c1d
Ryan Horn <rj.horn@gmail.com>
2022-11-14 07:50:09 -0700

    commit tree 0944e0870330928a2ed8d7721edef4fcd2d6cb1b

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function sayHiToHeadphonedRoommate(x){
+    if(x != x.toUpperCase()){
+        
+        return "I can\'t hear you!";
+    }
+
+    else if(x = x.toUpperCase){
+        return "YES INDEED!"
+    }
+
+    else if(x = "Let's have dinner together!"){
+        return "I would love to!";
+    }
+}
+
+function logShout(string){
+    console.log(string.toUpperCase);
+}
